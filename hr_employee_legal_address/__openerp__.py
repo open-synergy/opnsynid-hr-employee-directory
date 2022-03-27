2519c58e0cebdd09d7331a172c7497da43731385
Nurazmi <azmimr67@gmail.com>
2022-03-27 16:53:33 +0700

    [hr_employee_legal_address] 8.0.1.0.0

---
+++ hr_employee_legal_address/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author/
+{
+    "name": "Employee Legal Address",
+    "version": "8.0.1.0.0",
+    "category": "Human Resource",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia,OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "hr",
+    ],
+    "data": [
+        "views/hr_employee_views.xml",
+    ],
+}
---
+++ hr_employee_legal_address/models/hr_employee.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import fields, models
+
+
+class HrEmployee(models.Model):
+    _inherit = "hr.employee"
+
+    legal_address_id = fields.Many2one(
+        string="Legal Address",
+        comodel_name="res.partner",
+    )
+    legal_street = fields.Char(
+        string="Street",
+        related="legal_address_id.street",
+        store=True,
+    )
+    legal_street2 = fields.Char(
+        string="Street2",
+        related="legal_address_id.street2",
+        store=True,
+    )
+    legal_zip = fields.Char(
+        string="ZIP",
+        related="legal_address_id.zip",
+        store=True,
+    )
+    legal_city = fields.Char(
+        string="City",
+        related="legal_address_id.city",
+        store=True,
+    )
+    legal_state_id = fields.Many2one(
+        string="State",
+        comodel_name="res.country.state",
+        related="legal_address_id.state_id",
+        store=True,
+    )
+    legal_country_id = fields.Many2one(
+        string="Country",
+        comodel_name="res.country",
+        related="legal_address_id.country_id",
+        store=True,
+    )
+    legal_phone = fields.Char(
+        string="Phone",
+        related="legal_address_id.phone",
+        readonly=True,
+        store=True,
+    )

bd5e3745c78e5d58aaf9c35514845da7b5f506b8
Andhitia Rama <andhitia.r@gmail.com>
2022-04-08 08:30:33 +0700

    [hr_employee_legal_address] pre-commit fix

--- hr_employee_legal_address/models/hr_employee.py
+++ hr_employee_legal_address/models/hr_employee.py
@@ -3,7 +3,7 @@
 # Copyright 2022 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import fields, models, api
+from openerp import api, fields, models
 
 
 class HrEmployee(models.Model):
